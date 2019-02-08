38ceeabe3d02761ddb406cfe9a6ee9175eb913d1
hps3103 <ssingh.harpushpam@gmail.com>
2019-02-08 18:21:55 -0500

    6pm

--- c0732134_Week05/Program.cs
+++ c0732134_Week05/Program.cs
@@ -6,7 +6,33 @@ namespace c0732134_Week05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Elevator e = new Elevator();
+            e.run();
+        }
+    }
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string floorNumber;
+    }
+    class Elevator
+    {
+        Node firstFloor;
+        Node secondFloor;
+        Node thirdFloor;
+        Node fourthFloor;
+
+        public void run()
+        {
+            firstFloor = new Node();
+            firstFloor.floorNumber = "First Floor";
+            Console.WriteLine("The Floor Number is : " + firstFloor.floorNumber);
+            firstFloor.elevatorUp = secondFloor;
+            secondFloor.floorNumber = "Second Floor";
+            Console.WriteLine("The Floor Number is : " + secondFloor.floorNumber);
+            Console.ReadLine();
         }
     }
 }
